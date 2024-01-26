af95f48db60043362ace502a6bfce9c9e11a8aad
Elamathi26 <elamathi2611@gmail.com>
2024-01-26 12:52:07 +0530

    modified

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ request.open("GET","https://restcountries.com/v3.1/all")
 request.send ();
 request.onload = function () {  
     var res = JSON.parse (request.response);
-    console.log (res)
+    console.log(res);
+}
+for(var i =0;i<res.length;i++){
+    console.log(res[i].name.common)
 }
     
 
