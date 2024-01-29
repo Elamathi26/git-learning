7be88fe27a99cb5ffab51c5d5194473cbcf6c624
Elamathi26 <elamathi2611@gmail.com>
2024-01-29 09:34:11 +0530

    he

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ request.onload = function(){
 var res = JSON.parse(request.response);
 console.log(res);
 for (var i=0;i<res.length;i++){
-    console.log(res[i].name.common)
+ if(res[i].capital[0]){
+ console.log (res[i].capital[0]+" "+res[i].area)
+}
+ else{
+    console.log ("Invalid capital");
+ }
 }
 }
